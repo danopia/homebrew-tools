fcc339cf4894b476ddf7d6a60f17383956795a4f
Daniel Lamando <dan@danopia.net>
2021-02-28 22:03:23 +0000

    saml2aws: add 0.0.1 bottle.

--- Formula/saml2aws.rb
+++ Formula/saml2aws.rb
@@ -6,6 +6,12 @@ class Saml2aws < Formula
   revision: "6c6e0f7bff27ec3b60ca862f086bb5109463ba74"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/danopia/homebrew-tools/releases/download/saml2aws-0.0.1"
+    sha256 cellar: :any_skip_relocation, catalina:     "d74f2e5303c3d6283b4d3fef1c75c2571c50cff3f0ff83fcaef100284e2d6d5a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "bcfab6ce9d566cec16cf03987de9e06a0ffb4e2dc8d0ad3e09a325c2c975d8ae"
+  end
+
   depends_on "go" => :build
 
   def install
